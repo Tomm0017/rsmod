b267f886998fb1bec07351f179090c8d0b266583
rsmod <rspsmods@gmail.com>
2019-01-05 21:24:33 -0500

    - Readding core OSRS plugin files

---
+++ game/src/main/kotlin/gg/rsmod/plugins/osrs/BonusSlot.kt
@@ -0,0 +1,23 @@
+package gg.rsmod.plugins.osrs
+
+/**
+ * @author Tom <rspsmods@gmail.com>
+ */
+enum class BonusSlot(val id: Int) {
+    ATTACK_STAB(id = 0),
+    ATTACK_SLASH(id = 1),
+    ATTACK_CRUSH(id = 2),
+    ATTACK_MAGIC(id = 3),
+    ATTACK_RANGED(id = 4),
+
+    DEFENCE_STAB(id = 5),
+    DEFENCE_SLASH(id = 6),
+    DEFENCE_CRUSH(id = 7),
+    DEFENCE_MAGIC(id = 8),
+    DEFENCE_RANGED(id = 9),
+
+    STRENGTH(id = 10),
+    RANGED_STRENGTH(id = 11),
+    MAGIC_DAMAGE(id = 12),
+    PRAYER(id = 13)
+}
---
+++ game/src/main/kotlin/gg/rsmod/plugins/osrs/Equipment.kt
@@ -0,0 +1,19 @@
+package gg.rsmod.plugins.osrs
+
+/**
+ * @author Tom <rspsmods@gmail.com>
+ */
+enum class Equipment(val id: Int) {
+    HEAD(id = 0),
+    CAPE(id = 1),
+    AMULET(id = 2),
+    WEAPON(id = 3),
+    CHEST(id = 4),
+    SHIELD(id = 5),
+    LEGS(id = 7),
+    GLOVES(id = 9),
+    BOOTS(id = 10),
+    BEARD(id = 11),
+    RING(id = 12),
+    AMMO(id = 13)
+}

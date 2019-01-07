6831336b087f5c61f4e72d6b7f55b7f8138d7dd1
rsmod <rspsmods@gmail.com>
2019-01-07 07:08:49 -0500

    - Start of banking - Added GeneralHelper.ItemContainer.swap which swaps items in between item containers and copies attributes when appropriate

--- game/src/main/kotlin/gg/rsmod/plugins/GeneralHelper.kt
+++ game/src/main/kotlin/gg/rsmod/plugins/GeneralHelper.kt
@@ -1,5 +1,8 @@
 package gg.rsmod.plugins
 
+import gg.rsmod.game.model.container.ItemContainer
+import gg.rsmod.game.model.item.Item
+
 /**
  * @author Tom <rspsmods@gmail.com>
  */
@@ -9,4 +12,55 @@ fun String.plural(amount: Int): String {
         return this
     }
     return if (amount != 1) this + "s" else this
+}
+
+fun ItemContainer.swap(to: ItemContainer, item: Item, beginSlot: Int): Int {
+    check(item.amount > 0)
+
+    val copy = Item(item)
+
+    val removal = remove(item.id, item.amount, assureFullRemoval = true, beginSlot = beginSlot)
+    if (removal.hasFailed()) {
+        return 0
+    }
+    val addition = to.add(copy.id, copy.amount, assureFullInsertion = false)
+    if (addition.completed > 0) {
+        /**
+         * If at least one item was successfully added to [to], we copy the attributes
+         * from the copy of [item].
+         */
+        addition.items.firstOrNull()?.copyAttr(copy)
+    } else {
+        /**
+         * If not a single item could be added, we refund the original item back
+         * to [ItemContainer.this].
+         */
+        val refund = add(copy.id, copy.amount, assureFullInsertion = true, beginSlot = beginSlot)
+        refund.items.firstOrNull()?.copyAttr(copy)
+        return 0
+    }
+    return addition.completed
+}
+
+/**
+ * Similar to other [swap] method, however this does not copy any attributes.
+ */
+fun ItemContainer.swap(to: ItemContainer, item: Int, amount: Int, beginSlot: Int): Int {
+    val copy = Item(item, amount)
+
+    val removal = remove(item, amount, assureFullRemoval = true, beginSlot = beginSlot)
+    if (removal.hasFailed()) {
+        return 0
+    }
+    val addition = to.add(copy.id, copy.amount, assureFullInsertion = false)
+    if (addition.completed <= 0) {
+        /**
+         * If not a single item could be added, we refund the original item back
+         * to [ItemContainer.this].
+         */
+        val refund = add(copy.id, copy.amount, assureFullInsertion = true, beginSlot = beginSlot)
+        refund.items.firstOrNull()?.copyAttr(copy)
+        return 0
+    }
+    return addition.completed
 }
--- game/src/main/kotlin/gg/rsmod/plugins/osrs/content/inter/Bank.kt
+++ game/src/main/kotlin/gg/rsmod/plugins/osrs/content/inter/Bank.kt
@@ -1,31 +1,93 @@
 package gg.rsmod.plugins.osrs.content.inter
 
-import gg.rsmod.game.model.item.Item
 import gg.rsmod.game.plugin.PluginRepository
 import gg.rsmod.game.plugin.ScanPlugins
-import gg.rsmod.plugins.getInteractingSlot
-import gg.rsmod.plugins.player
+import gg.rsmod.plugins.*
 
 /**
  * @author Tom <rspsmods@gmail.com>
  */
 object Bank {
 
+    private const val BANK_INTERFACE_ID = 12
     private const val INV_INTERFACE_ID = 15
     private const val INV_INTERFAC_CHILD = 3
+
+    private const val QUANTITY_VARBIT = 6590
 
     @JvmStatic
     @ScanPlugins
     fun register(r: PluginRepository) {
+        intArrayOf(28, 30, 32, 34, 36).forEach { quantity ->
+            r.bindButton(parent = BANK_INTERFACE_ID, child = quantity) {
+                val state = (quantity - 28) / 2
+                it.player().setVarbit(QUANTITY_VARBIT, state)
+            }
+        }
         r.bindButton(parent = INV_INTERFACE_ID, child = INV_INTERFAC_CHILD) {
             val p = it.player()
 
+            val opt = it.getInteractingOption()
             val slot = it.getInteractingSlot()
             val item = p.inventory[slot] ?: return@bindButton
-            val copyItem = Item(item)
 
-            if (p.inventory.remove(item.id, item.amount, beginSlot = slot).hasSucceeded()) {
-                p.bank.add(copyItem.id, copyItem.amount).items.first().copyAttr(copyItem)
+            if (opt == 9) {
+                // TODO(Tom): examine message
+                return@bindButton
+            }
+
+            val quantityVarbit = p.getVarbit(QUANTITY_VARBIT)
+            var amount = 0
+
+            if (quantityVarbit != 0) {
+                if (opt == 1) {
+                    amount = when (quantityVarbit) {
+                        1 -> 5
+                        2 -> 10
+                        3 -> -1 // X
+                        4 -> 0 // All
+                        else -> return@bindButton
+                    }
+                } else {
+                    amount = when (opt) {
+                        2 -> 1
+                        3 -> 5
+                        4 -> 10
+                        6 -> -1 // X
+                        else -> return@bindButton
+                    }
+                }
+            } else {
+                amount = when (opt) {
+                    1 -> 1
+                    3 -> 5
+                    4 -> 10
+                    6 -> -1 // X
+                    7 -> 0 // All
+                    else -> return@bindButton
+                }
+            }
+            if (amount == 0) {
+                amount = p.inventory.getItemCount(item.id)
+            } else if (amount == -1) {
+                // Prompt for X and swap in suspendable block
+                return@bindButton
+            }
+
+            var deposited = 0
+            for (i in 0 until p.inventory.capacity) {
+                val invItem = p.inventory[i] ?: continue
+                if (invItem.id == item.id) {
+                    if (invItem.amount + deposited <= amount) {
+                        deposited += p.inventory.swap(p.bank, invItem, beginSlot = i)
+                    } else {
+                        deposited += p.inventory.swap(p.bank, invItem.id, amount - deposited, beginSlot = i)
+                    }
+                }
+            }
+
+            if (deposited == 0) {
+                p.message("Bank full.")
             }
         }
     }

416e337dedd5c98475f007bf51aa5bab90bc16b2
rsmod <rspsmods@gmail.com>
2019-01-06 20:49:03 -0500

    - Finished PluginPacker!

--- tools/src/main/kotlin/gg/rsmod/tools/plugin/PluginPacker.kt
+++ tools/src/main/kotlin/gg/rsmod/tools/plugin/PluginPacker.kt
@@ -5,20 +5,36 @@ import net.lingala.zip4j.model.ZipParameters
 import net.lingala.zip4j.util.Zip4jConstants
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.streams.toList
 
 /**
  * @author Tom <rspsmods@gmail.com>
  */
 class PluginPacker {
 
-    /**
-     * Alias for [zipFiles]
-     */
-    fun compileSource(output: Path, paths: List<Path>) {
-        zipFiles(output, paths)
+    fun compileSource(pluginName: String, outputPath: Path, paths: List<Path>): Boolean {
+        return zipFiles(outputPath.resolve("$pluginName.zip"), paths, null)
     }
 
-    fun compileBinary(compilerPath: String, gameJar: String, plugin: Path, paths: List<Path>): Boolean {
+    fun compileBinary(compilerPath: String, gameJar: String, pluginName: String,
+                      outputPath: Path, paths: List<Path>): Boolean {
+        val tmpOutput = outputPath.resolve("$pluginName.tmp")
+        val output = outputPath.resolve("$pluginName.jar")
+
+        val success = compileKotlin(compilerPath, gameJar, tmpOutput, paths)
+        if (success) {
+            try {
+                zipFiles(output = output, paths = Files.walk(tmpOutput).toList(),
+                        removeParent = tmpOutput.toString().replace("\\", "/"))
+            } finally {
+                tmpOutput.toFile().deleteRecursively()
+            }
+            return true
+        }
+        return false
+    }
+
+    private fun compileKotlin(compilerPath: String, gameJar: String, plugin: Path, paths: List<Path>): Boolean {
         val splitPaths = paths.filter { it.fileName.toString().endsWith(".kt") }.joinToString(" ") { "\"$it\"" }
 
         val process = ProcessBuilder("$compilerPath/kotlinc.bat", "$splitPaths -classpath \"$gameJar\" -d \"$plugin\"")
@@ -29,22 +45,28 @@ class PluginPacker {
         return status.exitValue() == 0
     }
 
-    fun zipFiles(output: Path, paths: List<Path>, removeParent: String? = null) {
+    private fun zipFiles(output: Path, paths: List<Path>, removeParent: String?): Boolean {
         val zip = ZipFile(output.toAbsolutePath().toString())
         val params = ZipParameters()
         params.compressionMethod = Zip4jConstants.COMP_DEFLATE
         params.compressionLevel = Zip4jConstants.DEFLATE_LEVEL_ULTRA
-        paths.forEach { path ->
-            if (!Files.isDirectory(path)) {
-                val file = path.toFile()
-                params.rootFolderInZip = file.parent
-                if (removeParent != null) {
-                    params.rootFolderInZip = params.rootFolderInZip.replace(removeParent, "").substring(1)
-                }
-                if (!params.rootFolderInZip.contains("META-INF")) {
-                    zip.addFile(file, params)
+        try {
+            paths.forEach { path ->
+                if (!Files.isDirectory(path)) {
+                    val file = path.toFile()
+                    params.rootFolderInZip = file.parent
+                    if (removeParent != null) {
+                        params.rootFolderInZip = params.rootFolderInZip.replace(removeParent, "").substring(1)
+                    }
+                    if (!params.rootFolderInZip.contains("META-INF")) {
+                        zip.addFile(file, params)
+                    }
                 }
             }
+            return true
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
+        return false
     }
 }
--- tools/src/main/kotlin/gg/rsmod/tools/plugin/PluginPackerMain.kt
+++ tools/src/main/kotlin/gg/rsmod/tools/plugin/PluginPackerMain.kt
@@ -1,7 +1,9 @@
 package gg.rsmod.tools.plugin
 
 import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.HelpFormatter
 import org.apache.commons.cli.Options
+import org.apache.commons.cli.ParseException
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.streams.toList
@@ -11,37 +13,81 @@ import kotlin.streams.toList
  */
 object PluginPackerMain {
 
+    private enum class PackType {
+        ZIP,
+        JAR
+    }
+
     @JvmStatic fun main(vararg args: String) {
         val packer = PluginPacker()
 
         if (args.isNotEmpty()) {
             val options = Options()
-            //"C:/Program Files/kotlinc/bin/"
-            //"game/build/libs/game-0.0.1.jar"
-            options.addOption("c", true, "The path to your Kotlin compiler")
-            options.addOption("g", true, "The path to your game distribution jar")
-            options.addOption("s", true, "The path to the plugin files you would like to pack")
+            options.addOption("t", true, "The type of packing: <zip> or <jar>")
             options.addOption("p", true, "The name you would like to give your packed plugin")
+            options.addOption("s", true, "The path to the plugin files that you wish to pack")
+            options.addOption("c", true, "The path to your Kotlin compiler [only for <jar> packing]")
+            options.addOption("g", true, "The path to your Game jar [only for <jar> packing]")
 
             val parser = DefaultParser()
             val commands = parser.parse(options, args)
 
-            val pluginName = commands.getOptionValue('p')
-            val source = Paths.get(commands.getOptionValue('s'))
-            val output = Paths.get(".", "plugins").resolve("$pluginName.tmp")
+            if (commands.args.contains("help")) {
+                HelpFormatter().printHelp("<t> <c> <g> <s> <p>", options)
+                return
+            } else if (!commands.hasOption('t')) {
+                HelpFormatter().printHelp("<t> <c> <g> <s> <p>", options)
+                return
+            }
 
-            val success = packer.compileBinary(
-                    compilerPath = commands.getOptionValue('c'),
-                    gameJar = commands.getOptionValue('g'),
-                    plugin = output,
-                    paths = Files.walk(source).toList())
-            if (success) {
-                try {
-                    packer.zipFiles(output = Paths.get(".", "plugins").resolve("$pluginName.jar"),
-                            paths = Files.walk(output).toList(), removeParent = output.toString().replace("\\", "/"))
-                } finally {
-                    output.toFile().deleteRecursively()
+            val packType = when {
+                commands.getOptionValue('t').toLowerCase() == "zip" -> PackType.ZIP
+                commands.getOptionValue('t').toLowerCase() == "jar" -> PackType.JAR
+                else -> null
+            }
+
+            if (packType == null) {
+                HelpFormatter().printHelp("<t> <c> <g> <s> <p>", options)
+                return
+            }
+            val requiredOptions = when (packType) {
+                PackType.ZIP -> arrayOf(options.getOption("p"), options.getOption("s"))
+                PackType.JAR -> arrayOf(options.getOption("p"), options.getOption("s"), options.getOption("c"), options.getOption("g"))
+            }
+            if (requiredOptions.any { !commands.hasOption(it.opt) }) {
+                val formatter = HelpFormatter()
+                formatter.printHelp("<c> <g> <s> <p>", options)
+                return
+            }
+
+            try {
+                val pluginName = commands.getOptionValue('p')
+                val source = Paths.get(commands.getOptionValue('s'))
+                val output = Paths.get(".", "plugins")
+
+                if (packType == PackType.JAR) {
+                    val compilerPath = commands.getOptionValue('c')
+                    val gameJarPath = commands.getOptionValue('g')
+
+                    val compiler = Paths.get(compilerPath).resolve("kotlinc")
+                    val gameJar = Paths.get(gameJarPath)
+
+                    if (!Files.exists(compiler)) {
+                        error("Kotlin compiler could not be found in: $compiler")
+                    } else if (!Files.exists(gameJar)) {
+                        error("Game distribution jar could not be found in: $gameJar")
+                    }
+
+                    if (packer.compileBinary(compilerPath, gameJarPath, pluginName, output, Files.walk(source).toList())) {
+                        println("Plugin has been compiled and created as: ${output.resolve("$pluginName.jar")}")
+                    } else {
+                        println("Could not pack plugin! Make sure you have ")
+                    }
+                } else {
+                    packer.compileSource(pluginName, output, Files.walk(source).toList())
                 }
+            } catch (e: ParseException) {
+                println("Invalid arguments! Use -help for arg explanation.")
             }
         } else {
             // TODO(Tom): launch gui

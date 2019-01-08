7f928e97e745b1af8e9927825083195122161c5c
rsmod <rspsmods@gmail.com>
2019-01-08 14:07:27 -0500

    - Replacing last of content that uses magic item ids

--- game/src/main/kotlin/gg/rsmod/plugins/osrs/content/skills/woodcutting/WoodcuttingAxe.kt
+++ game/src/main/kotlin/gg/rsmod/plugins/osrs/content/skills/woodcutting/WoodcuttingAxe.kt
@@ -1,16 +1,18 @@
 package gg.rsmod.plugins.osrs.content.skills.woodcutting
 
+import gg.rsmod.plugins.osrs.api.Items
+
 /**
  * @author Tom <rspsmods@gmail.com>
  */
 enum class WoodcuttingAxe(val item: Int, val level: Int, val animation: Int) {
-    BRONZE(item = 1351, level = 1, animation = 879),
-    IRON(item = 1349, level = 1, animation = 877),
-    STEEL(item = 1353, level = 6, animation = 875),
-    BLACK(item = 1361, level = 11, animation = 873),
-    MITHRIL(item = 1355, level = 21, animation = 871),
-    ADAMANT(item = 1357, level = 31, animation = 869),
-    RUNE(item = 1359, level = 41, animation = 867),
-    DRAGON(item = 6739, level = 61, animation = 2846),
-    INFERNAL(item = 13241, level = 61, animation = 2117)
+    BRONZE(item = Items.BRONZE_AXE, level = 1, animation = 879),
+    IRON(item = Items.IRON_AXE, level = 1, animation = 877),
+    STEEL(item = Items.STEEL_AXE, level = 6, animation = 875),
+    BLACK(item = Items.BLACK_AXE, level = 11, animation = 873),
+    MITHRIL(item = Items.MITHRIL_AXE, level = 21, animation = 871),
+    ADAMANT(item = Items.ADAMANT_AXE, level = 31, animation = 869),
+    RUNE(item = Items.RUNE_AXE, level = 41, animation = 867),
+    DRAGON(item = Items.DRAGON_AXE, level = 61, animation = 2846),
+    INFERNAL(item = Items.INFERNAL_AXE, level = 61, animation = 2117)
 }

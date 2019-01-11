4643888430bb22957d6a300c86f384e6223a5c8a
rsmod <rspsmods@gmail.com>
2019-01-11 15:21:03 -0500

    - Binding unequipment buttons

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/osrs/content/equipment.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/osrs/content/equipment.kts
@@ -17,7 +17,17 @@ for (equipment in Equipment.values()) {
     }
 }
 
-//bindUnequip(Equipment.WEAPON)
+bindUnequip(Equipment.HEAD, child = 6)
+bindUnequip(Equipment.CAPE, child = 7)
+bindUnequip(Equipment.AMULET, child = 8)
+bindUnequip(Equipment.AMMO, child = 16)
+bindUnequip(Equipment.WEAPON, child = 9)
+bindUnequip(Equipment.CHEST, child = 10)
+bindUnequip(Equipment.SHIELD, child = 11)
+bindUnequip(Equipment.LEGS, child = 12)
+bindUnequip(Equipment.GLOVES, child = 13)
+bindUnequip(Equipment.BOOTS, child = 14)
+bindUnequip(Equipment.RING, child = 15)
 
 fun bindUnequip(equipment: Equipment, child: Int) {
     r.bindButton(parent = 387, child = child) {

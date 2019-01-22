da47479fb371734d58bb44fc738e04eaf8b735df
rsmod <rspsmods@gmail.com>
2019-01-22 10:14:51 -0500

    - Docs for BitStorage

--- game/src/main/kotlin/gg/rsmod/game/model/BitStorage.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/BitStorage.kt
@@ -4,17 +4,35 @@ import gg.rsmod.game.model.entity.Player
 import gg.rsmod.util.BitManipulation
 
 /**
+ * A storage for up to thirty-two (32) bits. This means you can have up to 32
+ * possible values stored in a single integer instead of say 32 separate booleans.
+ *
+ * @param key
+ * The attribute key used to store the packed bit values for this storage.
+ *
+ * If the given [AttributeKey] is persistent, the bits in this storage will
+ * be saved for any player that uses it.
+ *
  * @author Tom <rspsmods@gmail.com>
  */
 class BitStorage(val key: AttributeKey<Int>) {
 
     /**
-     * Use when this [BitStorage] should persist (save) on log-out.
+     * Constructs a new [BitStorage] with a persistent [AttributeKey], which means
+     * that any modification done per player will be persistent for said player.
      */
     constructor(persistenceKey: String) : this(AttributeKey<Int>(persistenceKey))
 
+    /**
+     * Get the unpacked value of the bits in range of [StorageBits.startBit]
+     * to [StorageBits.endBit].
+     */
     fun get(p: Player, bits: StorageBits): Int = BitManipulation.getBit(packed = get(p), startBit = bits.startBit, endBit = bits.endBit)
 
+    /**
+     * Sets the unpacked value of the bits in range of [StorageBits.startBit]
+     * to [StorageBits.endBit], to [value].
+     */
     fun set(p: Player, bits: StorageBits, value: Int) {
         set(p, BitManipulation.setBit(packed = get(p), startBit = bits.startBit, endBit = bits.endBit, value = value))
     }

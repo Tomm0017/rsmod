a5bd8669360776eecf11f1800b0cc09405d9c12d
rsmod <rspsmods@gmail.com>
2019-01-22 10:35:46 -0500

    - Docs for AttributeKey

--- game/src/main/kotlin/gg/rsmod/game/model/AttributeKey.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/AttributeKey.kt
@@ -6,6 +6,16 @@ import com.google.common.base.MoreObjects
  * An [AttributeKey] is a flexible key that can be used to represent any type of
  * value.
  *
+ * @param T
+ * The type of the value that this attribute will store.
+ *
+ * <strong>Note</strong>- do not use Double or Float if your key needs to be
+ * persistent ([persistenceKey] != null). Not all saving and loading systems
+ * would be able to differentiate doubles and ints when being loaded, so for
+ * compatibility reasons, it is preferable to use an [Int] instead and just
+ * multiply or divide it accordingly to represent a [Double] or [Float], if
+ * needed.
+ *
  * @author Tom <rspsmods@gmail.com>
  */
 @Suppress("unused")
@@ -17,8 +27,7 @@ class AttributeKey<T>(val persistenceKey: String? = null) {
         if (other !is AttributeKey<*>) {
             return false
         }
-        return if (persistenceKey != null) other.persistenceKey == persistenceKey
-                else super.equals(other)
+        return if (persistenceKey != null) other.persistenceKey == persistenceKey else super.equals(other)
     }
 
     override fun hashCode(): Int = persistenceKey?.hashCode() ?: super.hashCode()

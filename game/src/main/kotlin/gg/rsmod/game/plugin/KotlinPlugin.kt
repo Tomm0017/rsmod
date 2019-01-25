b6624147ed377d6314e3a8a9618dea6e1cd9d4b4
rsmod <rspsmods@gmail.com>
2019-01-25 05:08:59 -0500

    - Moved PluginRepositoryHelper functions to KotlinPlugin instead

--- game/src/main/kotlin/gg/rsmod/game/plugin/KotlinPlugin.kt
+++ game/src/main/kotlin/gg/rsmod/game/plugin/KotlinPlugin.kt
@@ -1,5 +1,7 @@
 package gg.rsmod.game.plugin
 
+import gg.rsmod.game.fs.def.NpcDef
+import gg.rsmod.game.fs.def.ObjectDef
 import gg.rsmod.game.model.World
 import kotlin.script.experimental.annotations.KotlinScript
 
@@ -7,4 +9,25 @@ import kotlin.script.experimental.annotations.KotlinScript
  * @author Tom <rspsmods@gmail.com>
  */
 @KotlinScript(displayName = "Kotlin Plugin", fileExtension = "kts")
-abstract class KotlinPlugin(val r: PluginRepository, val world: World)
+abstract class KotlinPlugin(val r: PluginRepository, val world: World) {
+
+    fun bindObjOption(obj: Int, option: String, plugin: Function1<Plugin, Unit>) {
+        val opt = option.toLowerCase()
+        val def = world.definitions.get(ObjectDef::class.java, obj)
+        val slot = def.options.filterNotNull().indexOfFirst { it.toLowerCase() == opt }
+
+        check(slot != -1) { "Option \"$option\" not found for object $obj [valid_options=${def.options.filterNotNull().filter { it.isNotBlank() }}]" }
+
+        r.bindObject(obj, slot, plugin)
+    }
+
+    fun bindNpcOption(npc: Int, option: String, plugin: Function1<Plugin, Unit>) {
+        val opt = option.toLowerCase()
+        val def = world.definitions.get(NpcDef::class.java, npc)
+        val slot = def.options.filterNotNull().indexOfFirst { it.toLowerCase() == opt }
+
+        check(slot != -1) { "Option \"$option\" not found for npc $npc [valid_options=${def.options.filterNotNull().filter { it.isNotBlank() }}]" }
+
+        r.bindNpc(npc, slot, plugin)
+    }
+}

16d7b7d2a6331ae29ace10c85906373641188c82
rsmod <rspsmods@gmail.com>
2019-03-18 10:23:14 -0400

    - Added OPLOC5 packet

--- game/src/main/kotlin/gg/rsmod/game/message/MessageDecoderSet.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/MessageDecoderSet.kt
@@ -55,6 +55,7 @@ class MessageDecoderSet {
         put(OpLoc2Message::class.java, OpLoc2Decoder(), OpLoc2Handler(), structures)
         put(OpLoc3Message::class.java, OpLoc3Decoder(), OpLoc3Handler(), structures)
         put(OpLoc4Message::class.java, OpLoc4Decoder(), OpLoc4Handler(), structures)
+        put(OpLoc5Message::class.java, OpLoc5Decoder(), OpLoc5Handler(), structures)
         put(OpLoc6Message::class.java, OpLoc6Decoder(), OpLoc6Handler(), structures)
 
         put(OpHeld1Message::class.java, OpHeld1Decoder(), OpHeld1Handler(), structures)
---
+++ game/src/main/kotlin/gg/rsmod/game/message/handler/OpLoc5Handler.kt
@@ -0,0 +1,59 @@
+package gg.rsmod.game.message.handler
+
+import gg.rsmod.game.action.ObjectPathAction
+import gg.rsmod.game.message.MessageHandler
+import gg.rsmod.game.message.impl.OpLoc5Message
+import gg.rsmod.game.model.EntityType
+import gg.rsmod.game.model.Tile
+import gg.rsmod.game.model.attr.INTERACTING_OBJ_ATTR
+import gg.rsmod.game.model.attr.INTERACTING_OPT_ATTR
+import gg.rsmod.game.model.entity.Client
+import gg.rsmod.game.model.entity.GameObject
+import gg.rsmod.game.model.entity.Player
+import gg.rsmod.game.model.priv.Privilege
+import java.lang.ref.WeakReference
+
+/**
+ * @author Tom <rspsmods@gmail.com>
+ */
+class OpLoc5Handler : MessageHandler<OpLoc5Message> {
+
+    override fun handle(client: Client, message: OpLoc5Message) {
+        /**
+         * If player can't move, we don't do anything.
+         */
+        if (!client.lock.canMove()) {
+            return
+        }
+
+        /**
+         * If tile is too far away, don't process it.
+         */
+        val tile = Tile(message.x, message.z, client.tile.height)
+        if (!tile.viewableFrom(client.tile, Player.TILE_VIEW_DISTANCE)) {
+            return
+        }
+
+        /**
+         * Get the region chunk that the object would belong to.
+         */
+        val chunk = client.world.chunks.getOrCreate(tile)
+        val obj = chunk.getEntities<GameObject>(tile, EntityType.STATIC_OBJECT, EntityType.DYNAMIC_OBJECT).firstOrNull { it.id == message.id } ?: return
+
+        log(client, "Object action 5: id=%d, x=%d, z=%d, movement=%d", message.id, message.x, message.z, message.movementType)
+
+        client.stopMovement()
+        client.closeInterfaceModal()
+        client.interruptQueues()
+        client.resetInteractions()
+
+        if (message.movementType == 1 && client.world.privileges.isEligible(client.privilege, Privilege.ADMIN_POWER)) {
+            val def = obj.getDef(client.world.definitions)
+            client.teleport(client.world.findRandomTileAround(obj.tile, radius = 1, centreWidth = def.width, centreLength = def.length) ?: obj.tile)
+        }
+
+        client.attr[INTERACTING_OPT_ATTR] = 5
+        client.attr[INTERACTING_OBJ_ATTR] = WeakReference(obj)
+        client.executePlugin(ObjectPathAction.walkPlugin)
+    }
+}

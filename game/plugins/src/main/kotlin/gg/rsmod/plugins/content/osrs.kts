695f357ad5bfb93368b13f1dea2ecfb36d39fc4d
Tom0017 <rspsmods@gmail.com>
2019-03-22 19:34:21 -0400

    Add inventory item swapping

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/osrs.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/osrs.kts
@@ -1,5 +1,8 @@
 package gg.rsmod.plugins.content
 
+import gg.rsmod.game.model.attr.INTERACTING_ITEM_SLOT
+import gg.rsmod.game.model.attr.OTHER_ITEM_SLOT_ATTR
+
 /**
  * Closing main modal for players.
  */
@@ -61,4 +64,18 @@ on_login {
      */
     player.sendRunEnergy(player.runEnergy.toInt())
     player.message("Welcome to ${player.world.gameContext.name}.", ChatMessageType.SERVER)
+}
+
+/**
+ * Logic for swapping items in inventory.
+ */
+on_component_item_swap(interfaceId = 149, component = 0) {
+    val srcSlot = player.attr[INTERACTING_ITEM_SLOT]!!
+    val dstSlot = player.attr[OTHER_ITEM_SLOT_ATTR]!!
+
+    val container = player.inventory
+
+    if (srcSlot in 0 until container.capacity && dstSlot in 0 until container.capacity) {
+        container.swap(srcSlot, dstSlot)
+    }
 }

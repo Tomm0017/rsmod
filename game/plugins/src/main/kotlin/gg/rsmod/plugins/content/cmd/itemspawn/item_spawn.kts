28b6b3c6b2498a82365ff3f71a08df5c9ade328e
Tom0017 <rspsmods@gmail.com>
2019-03-26 10:00:49 -0400

    Add spawn command

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/itemspawn/item_spawn.kts
@@ -0,0 +1,27 @@
+package gg.rsmod.plugins.content.cmd.itemspawn
+
+import gg.rsmod.game.model.priv.Privilege
+
+on_command("spawn", Privilege.ADMIN_POWER) {
+    player.queue {
+        val item = spawn() ?: return@queue
+        if (item.amount > 0) {
+            player.message("You have spawned ${item.amount} x ${item.getName(world.definitions)}.")
+        } else {
+            player.message("You don't have enough inventory space.")
+        }
+    }
+}
+
+suspend fun QueueTask.spawn(): Item? {
+    val item = searchItemInput("Select an item to spawn:")
+    val amount = when (options("1", "5", "X", "Max", title = "How many would you like to spawn?")) {
+        1 -> 1
+        2 -> 5
+        3 -> inputInt("Enter amount to spawn")
+        4 -> Int.MAX_VALUE
+        else -> return null
+    }
+    val add = player.inventory.add(item, amount, assureFullInsertion = false)
+    return Item(item, add.completed)
+}

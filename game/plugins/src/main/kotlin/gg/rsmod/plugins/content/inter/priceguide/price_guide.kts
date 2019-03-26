96558c66546d8345452d90a8ddc38f7ad69b6cef
Tom0017 <rspsmods@gmail.com>
2019-03-26 09:47:49 -0400

    Set queues as WEAK

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/priceguide/price_guide.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/priceguide/price_guide.kts
@@ -1,8 +1,7 @@
 package gg.rsmod.plugins.content.inter.priceguide
 
 on_button(interfaceId = 387, component = 19) {
-    val p = player
-    if (!p.lock.canInterfaceInteract()) {
+    if (!player.lock.canInterfaceInteract()) {
         return@on_button
     }
     PriceGuide.open(player)
@@ -13,13 +12,13 @@ on_interface_close(interfaceId = PriceGuide.INTERFACE_ID) {
 }
 
 on_button(interfaceId = PriceGuide.TAB_INTERFACE_ID, component = 0) {
-    player.queue {
+    player.queue(TaskPriority.WEAK) {
         PriceGuide.add(this, player.getInteractingSlot(), player.getInteractingOption())
     }
 }
 
 on_button(interfaceId = PriceGuide.INTERFACE_ID, component = 2) {
-    player.queue {
+    player.queue(TaskPriority.WEAK) {
         PriceGuide.remove(this, player.getInteractingSlot(), player.getInteractingOption())
     }
 }
@@ -29,7 +28,7 @@ on_button(interfaceId = PriceGuide.INTERFACE_ID, component = 10) {
 }
 
 on_button(interfaceId = PriceGuide.INTERFACE_ID, component = 5) {
-    player.queue {
+    player.queue(TaskPriority.WEAK) {
         val item = searchItemInput("Select an item to ask about its price:")
         PriceGuide.search(player, item)
     }

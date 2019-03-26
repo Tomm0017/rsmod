3e7b882b4e5da6c161db5815138bba37a31cd851
Tom0017 <rspsmods@gmail.com>
2019-03-26 02:26:56 -0400

    Add default implementation for weighted item set

---
+++ game/src/main/kotlin/gg/rsmod/game/model/weight/impl/WeightItem.kt
@@ -0,0 +1,15 @@
+package gg.rsmod.game.model.weight.impl
+
+import gg.rsmod.game.model.item.Item
+import gg.rsmod.game.model.weight.WeightNode
+import java.util.*
+
+/**
+ * @author Tom <rspsmods@gmail.com>
+ */
+class WeightItem(val item: Int, private val amount: Int = 1, private val maxAmount: Int = amount, weight: Int) : WeightNode<Item>(weight) {
+
+    constructor(item: Int, amount: IntRange, weight: Int) : this(item, amount.start, amount.last, weight)
+
+    override fun convert(random: Random): Item = Item(item, amount + random.nextInt((maxAmount - amount) + 1))
+}
---
+++ game/src/main/kotlin/gg/rsmod/game/model/weight/impl/WeightItemSet.kt
@@ -0,0 +1,9 @@
+package gg.rsmod.game.model.weight.impl
+
+import gg.rsmod.game.model.item.Item
+import gg.rsmod.game.model.weight.WeightNodeSet
+
+/**
+ * @author Tom <rspsmods@gmail.com>
+ */
+class WeightItemSet : WeightNodeSet<Item>()

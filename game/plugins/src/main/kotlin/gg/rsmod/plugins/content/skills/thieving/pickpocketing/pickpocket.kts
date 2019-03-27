db5741f358ae8f2d099575f2231120ea954cc2de
Jacob Leonard <lethalleonard@gmail.com>
2019-03-27 08:30:28 -0400

    Refactored some checks to if(condition){continue} format

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/thieving/pickpocketing/pickpocket.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/thieving/pickpocketing/pickpocket.kts
@@ -3,63 +3,55 @@ package gg.rsmod.plugins.content.skills.thieving.pickpocketing
 import gg.rsmod.plugins.content.combat.isAttacking
 import gg.rsmod.plugins.content.combat.isBeingAttacked
 
-//for each npc in the enum
 PickpocketInfo.values().forEach { npcClass ->
-
-    //for each npc id in the current enum
     npcClass.ids.forEach { npcID ->
+        if (!world.definitions.get(NpcDef::class.java, npcID).options.contains("Pickpocket")) {
+            return@forEach
+        }
+        on_npc_option(npc = npcID, option = "pickpocket") {
 
-        //Extra check to ensure that the npc is pickpocket-able
-        if (world.definitions.get(NpcDef::class.java, npcID).options.contains("Pickpocket")) {
+            player.queue {
+                val thievLvl: Int = player.getSkills().getCurrentLevel(Skills.THIEVING)
+                if (thievLvl < npcClass.lvl) {
+                    player.message("You need level ${npcClass.lvl} to pickpocket a ${npcClass.npcName}.")
+                    return@queue
+                }
+                if (player.isAttacking() || player.isBeingAttacked()) {
+                    player.message("You can't pickpocket while in combat!")
+                    return@queue
+                }
+                if (!player.inventory.hasSpace) {
+                    player.message("You don't have enough inventory space to pickpocket!")
+                    return@queue
+                }
 
-            //Registers what to do with pickpocketing option
-            on_npc_option(npc = npcID, option = "pickpocket") {
+                //pickpocketing animation and starting message
+                player.animate(881)
+                player.message("You attempt to pickpocket the ${npcClass.npcName}...")
 
-                //accesses the player queue
-                player.queue {
-                    if (!player.isAttacking() && !player.isBeingAttacked()) {
-                        if (player.inventory.hasSpace) {
-                            //checks if the player has the required level for the npc
-                            val thievLvl: Int = player.getSkills().getCurrentLevel(Skills.THIEVING)
-                            if (thievLvl >= npcClass.lvl) {
+                //wait 3 game cycles
+                player.lock = LockState.FULL_WITH_ITEM_INTERACTION
+                wait(3)
+                player.lock = LockState.NONE
 
-                                //pickpocketing animation and starting message
-                                player.animate(881)
-                                player.message("You attempt to pickpocket the ${npcClass.npcName}...")
+                //determine if the pickpocket was successful or not by "if random number is within success chances"
+                val cap = if (player.hasEquipped(EquipmentType.GLOVES, Items.GLOVES_OF_SILENCE)) 5 else 0
+                if (thievLvl.interpolate(55, 95, npcClass.lvl, 99, 100 - cap)) {
 
-                                //wait 3 game cycles
-                                player.lock = LockState.FULL_WITH_ITEM_INTERACTION
-                                wait(3)
-                                player.lock = LockState.NONE
+                    player.message("...and you succeed!")
+                    val reward = npcClass.rewards.getRandom()
+                    player.inventory.add(reward)
+                    player.addXp(Skills.THIEVING, npcClass.exp)
 
-                                //determine if the pickpocket was successful or not by "if random number is within success chances"
-                                val cap = if (player.hasEquipped(EquipmentType.GLOVES, Items.GLOVES_OF_SILENCE)) 5 else 0
-                                if (thievLvl.interpolate(55, 95, npcClass.lvl, 99, 100 - cap)) {
+                } else {
+                    //if failed, sends relevant messages
+                    player.message("...and you have failed.")
 
-                                    player.message("...and you succeed!")
-                                    val reward = npcClass.rewards.getRandom()
-                                    player.inventory.add(reward)
-                                    player.addXp(Skills.THIEVING, npcClass.exp)
+                    //damages player for a value in the npc's damage range
+                    player.hit(npcClass.damage.random(), HitType.HIT)
 
-                                } else {
-                                    //if failed, sends relevant messages
-                                    player.message("...and you have failed.")
-
-                                    //damages player for a value in the npc's damage range
-                                    player.hit(npcClass.damage.random(), HitType.HIT)
-
-                                    //stuns the player then waits til the stun is done to continue
-                                    player.stun(npcClass.stunTicks)
-                                }
-                            } else {
-                                player.message("You need level ${npcClass.lvl} to pickpocket a ${npcClass.npcName}.")
-                            }
-                        } else {
-                            player.message("You don't have enough inventory space to pickpocket!")
-                        }
-                    }else{
-                        player.message("You can't pickpocket while in combat!")
-                    }
+                    //stuns the player then waits til the stun is done to continue
+                    player.stun(npcClass.stunTicks)
                 }
             }
         }

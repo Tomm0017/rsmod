48661ed6a242e2668a7de32795272f3dac39f5b0
Tom0017 <rspsmods@gmail.com>
2019-03-30 20:46:24 -0400

    Make sure npc can be attacked

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/Combat.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/Combat.kt
@@ -163,7 +163,7 @@ object Combat {
             if (!target.isSpawned()) {
                 return false
             }
-            if (target.combatDef.hitpoints <= 0) {
+            if (target.def.isAttackable() || target.combatDef.hitpoints <= 0) {
                 (pawn as? Player)?.message("You can't attack this npc.")
                 return false
             }

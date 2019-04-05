d9743ae660f9eca12c776451d0ed6a3a477799fb
HolyRSPS <dagreenrs@gmail.com>
2019-04-05 13:28:27 +0300

    Fixed world map packet, only admins can now use it.

--- game/src/main/kotlin/gg/rsmod/game/message/handler/ClickWorldMapHandler.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/handler/ClickWorldMapHandler.kt
@@ -4,6 +4,7 @@ import gg.rsmod.game.message.MessageHandler
 import gg.rsmod.game.message.impl.WorldMapClickMessage
 import gg.rsmod.game.model.Tile
 import gg.rsmod.game.model.entity.Client
+import gg.rsmod.game.model.priv.Privilege
 
 /**
  * @author HolyRSPS <dagreenrs@gmail.com>
@@ -14,7 +15,9 @@ class ClickWorldMapHandler : MessageHandler<WorldMapClickMessage> {
         val x = message.data shr 14 and 0x3FFF
         val y = message.data and 0x3FFF
         val z = message.data shr 28
-        log(client, "Click world map: x=%d, y=%d, z=%d", x, y, z)
-        client.moveTo(Tile(x, y, z))
+        if (client.world.privileges.isEligible(client.privilege, Privilege.ADMIN_POWER)) {
+            log(client, "Click world map: x=%d, y=%d, z=%d", x, y, z)
+            client.moveTo(Tile(x, y, z))
+        }
     }
 }

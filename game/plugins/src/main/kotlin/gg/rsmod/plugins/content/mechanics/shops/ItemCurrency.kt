0e19a9b7020b2e51077a0fbc9acbc358a1650296
Tom0017 <rspsmods@gmail.com>
2019-04-08 01:52:59 -0400

    Fix bug when non-stock item is sold

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/shops/ItemCurrency.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/shops/ItemCurrency.kt
@@ -133,8 +133,8 @@ open class ItemCurrency(private val currencyItem: Int, private val singularCurre
         }
 
         val shopSlot = shop.items.indexOfFirst { it?.item == unnoted }
-        val shopItem = shop.items[shopSlot]
-        val count = if (shopSlot != -1) shopItem?.currentAmount ?: 0 else 0
+        val shopItem = if (shopSlot != -1) shop.items[shopSlot] else null
+        val count = shopItem?.currentAmount ?: 0
 
         val amount = Math.min(Math.min(p.inventory.getItemCount(item.id), amt), Int.MAX_VALUE - count)
 

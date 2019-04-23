28529d1d8199e7c9d7a3b913ab4e02abd35c1a3a
Tom0017 <rspsmods@gmail.com>
2019-04-23 14:39:35 -0400

    Implement OPHELDT message

--- game/src/main/kotlin/gg/rsmod/game/message/handler/OpHeldTHandler.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/handler/OpHeldTHandler.kt
@@ -2,7 +2,11 @@ package gg.rsmod.game.message.handler
 
 import gg.rsmod.game.message.MessageHandler
 import gg.rsmod.game.message.impl.OpHeldTMessage
+import gg.rsmod.game.model.attr.INTERACTING_ITEM
+import gg.rsmod.game.model.attr.INTERACTING_ITEM_ID
+import gg.rsmod.game.model.attr.INTERACTING_ITEM_SLOT
 import gg.rsmod.game.model.entity.Client
+import java.lang.ref.WeakReference
 
 /**
  * @author Tom <rspsmods@gmail.com>
@@ -10,6 +14,39 @@ import gg.rsmod.game.model.entity.Client
 class OpHeldTHandler : MessageHandler<OpHeldTMessage> {
 
     override fun handle(client: Client, message: OpHeldTMessage) {
-        // TODO: handle magic on item
+        val fromComponentHash = message.fromComponentHash
+        val fromInterfaceId = fromComponentHash shr 16
+        val fromComponent = fromComponentHash and 0xFFFF
+
+        val toComponentHash = message.toComponentHash
+        val toInterfaceId = toComponentHash shr 16
+        val toComponent = toComponentHash and 0xFFFF
+
+        val itemId = message.item
+        val itemSlot = message.itemSlot
+        val unknown = message.spellSlot
+
+        val item = client.inventory[itemSlot] ?: return
+
+        if (item.id != itemId) {
+            return
+        }
+
+        if (!client.lock.canInterfaceInteract()) {
+            return
+        }
+
+        log(client, "Magic spell on item: from_component=[%d,%d], to_component=[%d,%d], unknown=%d, item=%d, item_slot=%d",
+                fromInterfaceId, fromComponent, toInterfaceId, toComponent, unknown, itemId, itemSlot)
+
+        client.attr[INTERACTING_ITEM] = WeakReference(item)
+        client.attr[INTERACTING_ITEM_ID] = itemId
+        client.attr[INTERACTING_ITEM_SLOT] = itemSlot
+
+        val handled = client.world.plugins.executeSpellOnItem(client, fromComponentHash, toComponentHash)
+        if (!handled && client.world.devContext.debugMagicSpells) {
+            client.message("Unhandled spell on item: [item=[${item.id}, ${item.amount}], item_slot=$itemSlot, unknown=$unknown " +
+                    "from_component=[$fromInterfaceId, $fromComponent], to_component=[$toInterfaceId, $toComponent]]")
+        }
     }
 }

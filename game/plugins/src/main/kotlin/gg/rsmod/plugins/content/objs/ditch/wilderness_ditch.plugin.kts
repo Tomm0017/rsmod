a14ebb07870b8fd1e1af0008222062591f93b2c4
Tom0017 <rspsmods@gmail.com>
2019-05-27 05:38:43 -0400

    Add support for west-east ditch crossing

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/ditch/wilderness_ditch.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/ditch/wilderness_ditch.plugin.kts
@@ -2,22 +2,40 @@ package gg.rsmod.plugins.content.objs.ditch
 
 on_obj_option(23271, "cross") {
     val ditch = player.getInteractingGameObj()
-    val southOfDitch = player.tile.z < ditch.tile.z
+    val sideCross = player.tile.z == ditch.tile.z
 
-    val movement = if (southOfDitch) {
-        ForcedMovement.of(player.tile, ditch.tile.step(Direction.NORTH, 2), clientDuration1 = 33,
-                clientDuration2 = 60, directionAngle = Direction.NORTH.angle)
+    val endTile: Tile
+    val directionAngle: Int
+
+    if (sideCross) {
+        val westOfDitch = player.tile.x < ditch.tile.x
+
+        if (westOfDitch) {
+            endTile = ditch.tile.step(Direction.EAST, 2)
+            directionAngle = Direction.EAST.angle
+        } else {
+            endTile = ditch.tile.step(Direction.WEST, 1)
+            directionAngle = Direction.WEST.angle
+        }
     } else {
-        ForcedMovement.of(player.tile, ditch.tile.step(Direction.SOUTH, 1), clientDuration1 = 33,
-                clientDuration2 = 60, directionAngle = Direction.SOUTH.angle)
+        val southOfDitch = player.tile.z < ditch.tile.z
+
+        if (southOfDitch) {
+            endTile = ditch.tile.step(Direction.NORTH, 2)
+            directionAngle = Direction.NORTH.angle
+        } else {
+            endTile = ditch.tile.step(Direction.SOUTH, 1)
+            directionAngle = Direction.SOUTH.angle
+        }
     }
 
+    val movement = ForcedMovement.of(player.tile, endTile, clientDuration1 = 33, clientDuration2 = 60, directionAngle = directionAngle)
     player.crossDitch(movement)
 }
 
 fun Player.crossDitch(movement: ForcedMovement) {
     queue {
         playSound(2452)
-        forceMove(this, movement, 6132)
+        forceMove(this, movement, animation = 6132)
     }
 }

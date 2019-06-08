90b149b2c62e788c167fe4c5bd8d3eee550635d1
HolyRSPS <dagreenrs@gmail.com>
2019-06-08 21:04:00 +0300

    Changed current time to on_timer.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/currenttime/current_time.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/currenttime/current_time.plugin.kts
@@ -1,19 +1,22 @@
 package gg.rsmod.plugins.content.inter.currenttime
 
-import gg.rsmod.game.model.interf.DisplayMode
-import java.util.Calendar
+import java.util.*
 
-DisplayMode.values.forEach { mode ->
-    val child = when (mode) {
-        DisplayMode.RESIZABLE_NORMAL -> 35
-        DisplayMode.RESIZABLE_LIST -> 35
-        DisplayMode.FIXED -> 31
-        else -> return@forEach
-    }
-    on_button(interfaceId = getDisplayComponentId(mode), component = child) {
-        val calendar = Calendar.getInstance()
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
-        val minute = calendar.get(Calendar.MINUTE)
-        player.setVarbit(id = 8354, value = ((hour * 60) + minute))
-    }
+val time = TimerKey()
+
+on_login {
+    setClanTime(player = player)
+}
+
+on_timer(time) {
+    setClanTime(player = player)
+}
+
+fun setClanTime(player : Player) {
+    val calendar = Calendar.getInstance()
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute = calendar.get(Calendar.MINUTE)
+    val second = calendar.get(Calendar.SECOND)
+    player.timers[time] = 60 - second
+    player.setVarbit(id = 8354, value = ((hour * 60) + minute))
 }

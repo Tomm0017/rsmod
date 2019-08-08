686378eb50ebb70da4e833a4807869db6a3b69c6
pitchblac23 <tylermincks@hotmail.com>
2019-08-08 00:54:07 -0500

    Add files via upload

--- game/src/main/kotlin/gg/rsmod/game/action/NpcDeathAction.kt
+++ game/src/main/kotlin/gg/rsmod/game/action/NpcDeathAction.kt
@@ -3,13 +3,17 @@ package gg.rsmod.game.action
 import gg.rsmod.game.fs.def.AnimDef
 import gg.rsmod.game.model.LockState
 import gg.rsmod.game.model.attr.KILLER_ATTR
+import gg.rsmod.game.model.entity.GroundItem
 import gg.rsmod.game.model.entity.Npc
 import gg.rsmod.game.model.entity.Player
+import gg.rsmod.game.model.item.Item
 import gg.rsmod.game.model.queue.QueueTask
 import gg.rsmod.game.model.queue.TaskPriority
 import gg.rsmod.game.plugin.Plugin
 import gg.rsmod.game.service.log.LoggerService
+import kotlinx.coroutines.channels.consumesAll
 import java.lang.ref.WeakReference
+import kotlin.random.Random
 
 /**
  * This class is responsible for handling npc death events.
@@ -55,6 +59,65 @@ object NpcDeathAction {
         npc.animate(-1)
 
         world.plugins.executeNpcDeath(npc)
+        if(npc.dropTables.always_table.items!!.isEmpty()) {
+            if (npc.respawns) {
+                npc.invisible = true
+                npc.reset()
+                wait(respawnDelay)
+                npc.invisible = false
+                world.plugins.executeNpcSpawn(npc)
+            } else {
+                world.remove(npc)
+            }
+        }
+        if(npc.dropTables.always_table.items!!.isNotEmpty()) {
+            val _always_table_random = (0 until npc.dropTables.always_table.items!!.size).random()
+            val _always_table_item = npc.dropTables.always_table.items!![_always_table_random]
+            var _always_table_quanity = 0
+
+            if (_always_table_item.quanityMin == _always_table_item.quanityMax) {
+                _always_table_quanity = _always_table_item.quanityMax
+            } else {
+                _always_table_quanity = (_always_table_item.quanityMin until _always_table_item.quanityMax).random()
+            }
+
+            world.spawn(item = GroundItem(_always_table_item.itemId, _always_table_quanity, npc.tile))
+        }
+
+        var _table = npc.dropTables.common_table
+
+        var _rolls = npc.dropTables.rolls
+        var _roll_count = 0
+
+        while(_roll_count < _rolls) {
+            var r: Double = Random.nextDouble(0.0, 100.0)
+            if(r <= npc.dropTables.veryrare_table.percentage && npc.dropTables.veryrare_table.items!!.isNotEmpty()) {
+                _table = npc.dropTables.veryrare_table
+            } else if(r <= npc.dropTables.rare_table.percentage && npc.dropTables.rare_table.items!!.isNotEmpty()) {
+                _table = npc.dropTables.rare_table
+            } else if(r <= npc.dropTables.uncommon_table.percentage && npc.dropTables.uncommon_table.items!!.isNotEmpty()) {
+                _table = npc.dropTables.uncommon_table
+            }
+
+            if(npc.dropTables.common_table.items!!.size == 0 && npc.dropTables.uncommon_table.items!!.size == 0 && npc.dropTables.rare_table.items!!.size == 0 && npc.dropTables.veryrare_table.items!!.size == 0) {
+                break
+            }
+
+            if(_table.items!!.size > 0) {
+                val _item_random = Random.nextInt(0, _table.items!!.size)
+                val _item = _table.items!![_item_random]
+                var _item_quanity = 0
+
+                if(_item.quanityMin == _item.quanityMax) {
+                    _item_quanity = _item.quanityMax
+                } else {
+                    _item_quanity = Random.nextInt(_item.quanityMin, _item.quanityMax)
+                }
+
+                world.spawn(item = GroundItem(_item.itemId, _item_quanity, npc.tile))
+                _roll_count++
+            }
+        }
 
         if (npc.respawns) {
             npc.invisible = true

0a2bc300fbfee10f7540c079edbd52739bf03de0
pitchblac23 <tylermincks@hotmail.com>
2019-08-08 01:38:27 -0500

    Add files via upload

---
+++ game/src/main/kotlin/gg/rsmod/game/message/impl/IfModelOpMessage.kt
@@ -0,0 +1,8 @@
+package gg.rsmod.game.message.impl
+
+import gg.rsmod.game.message.Message
+
+/**
+ * @author bmyte <bmytescape@gmail.com>
+ */
+data class IfModelOpMessage(val component: Int) : Message

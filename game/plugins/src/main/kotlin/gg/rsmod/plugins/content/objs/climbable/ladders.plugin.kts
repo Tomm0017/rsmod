4f23ee5b7a4c6228708937b9e990d0989887ffa3
pitchblac23 <tylermincks@hotmail.com>
2019-08-12 17:35:23 -0500

    added woodcutting guild ladders

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/climbable/ladders.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/climbable/ladders.plugin.kts
@@ -1,16 +1,31 @@
 package gg.rsmod.plugins.content.objs.climbable
 
-private val LADDER = 12964
-private val LADDER2 = 12965
-private val LADDER3 = 12966
+fun faceWest(player: Player) {
+    val direction : Direction = Direction.WEST
+    player.faceTile(player.tile.transform(direction.getDeltaX(), direction.getDeltaZ()))
+}
+fun faceEast(player: Player) {
+    val direction : Direction = Direction.EAST
+    player.faceTile(player.tile.transform(direction.getDeltaX(), direction.getDeltaZ()))
+}
+fun moveTo (player: Player, int: Int) {
+    val obj = player.getInteractingGameObj()
+    player.queue {
+        player.walkTo(this, obj.tile)
+        wait(3)
+        player.animate(int)
+        wait(2)
+    }
+}
 
-/**Lumby staircase floor*/
-on_obj_option(obj = LADDER, option = "climb-up") {
+/**Windmill bottom floor*/
+on_obj_option(Objs.LADDER_12964, option = "climb-up") {
     player.animate(828)
     player.moveTo(player.tile.x, player.tile.z, 1)
 }
 
-/**Lumby staircase middle*/
+/**Windmill middle floor*/
+private val LADDER2 = 12965
 on_obj_option(obj = LADDER2, option = "climb") {
     player.queue {
         when (options("Climb Up.", "Climb Down.", title = "Climb up or down the ladder?")) {
@@ -25,16 +40,43 @@ on_obj_option(obj = LADDER2, option = "climb") {
         }
     }
 }
-    on_obj_option(obj = LADDER2, option = "climb-up") {
-        player.animate(828)
-        player.moveTo(player.tile.x, player.tile.z, 2)
-    }
-    on_obj_option(obj = LADDER2, option = "climb-down") {
-        player.animate(828)
-        player.moveTo(player.tile.x, player.tile.z, 0)
-    }
+on_obj_option(obj = LADDER2, option = "climb-up") {
+    player.animate(828)
+    player.moveTo(player.tile.x, player.tile.z, 2)
+}
+on_obj_option(obj = LADDER2, option = "climb-down") {
+    player.animate(828)
+    player.moveTo(player.tile.x, player.tile.z, 0)
+}
 
-on_obj_option(obj = LADDER3, option = "climb-down") {
+/**Windmill top floor*/
+on_obj_option(Objs.LADDER_12966, option = "climb-down") {
     player.animate(828)
     player.moveTo(player.tile.x, player.tile.z, 1)
 }
+
+/**Wood Cutting Guild ladders*/
+on_obj_option(obj = Objs.ROPE_LADDER_28857, option = "climb-up") {
+    player.queue {
+        moveTo(player, 828)
+        wait(2)
+        if (player.tile.x == 1566 && player.tile.z == 3483 || player.tile.x == 1566 && player.tile.z == 3493) {
+            faceEast(player)
+            player.moveTo(player.tile.x + 2, player.tile.z, 1)
+        } else
+            faceWest(player)
+            player.moveTo(player.tile.x - 1, player.tile.z, 1)
+    }
+}
+on_obj_option(obj = Objs.ROPE_LADDER_28858, option = "climb-down") {
+    val obj = player.getInteractingGameObj()
+    player.queue {
+        moveTo(player, 828)
+        wait(2)
+        player.faceTile(obj.tile)
+        if (player.tile.x == 1567 && player.tile.z == 3483 || player.tile.x == 1567 && player.tile.z == 3493) {
+            player.moveTo(player.tile.x - 1, player.tile.z, 0)
+        } else player.moveTo(player.tile.x + 1, player.tile.z, 0)
+    }
+}
+
--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/climbable/stairs.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/climbable/stairs.plugin.kts
@@ -1,15 +1,12 @@
 package gg.rsmod.plugins.content.objs.climbable
 
-private val STAIRCASE = 16671
-private val STAIRCASE2 = 16672
-private val STAIRCASE3 = 16673
-
 /**Lumby staircase floor*/
-on_obj_option(obj = STAIRCASE, option = "climb-up") {
+on_obj_option(Objs.STAIRCASE_16671, option = "climb-up") {
     player.moveTo(3205, 3209, 1)
 }
 
 /**Lumby staircase middle*/
+private val STAIRCASE2 = 16672
 on_obj_option(obj = STAIRCASE2, option = "climb") {
     player.queue {
         when (options("Climb up the stairs.", "Climb down the stairs.", title = "Climb up or down the stairs?")) {
@@ -26,6 +23,6 @@ on_obj_option(obj = STAIRCASE2, option = "climb-down") {
 }
 
 /**Lumby staircase top*/
-on_obj_option(obj = STAIRCASE3, option = "climb-down") {
+on_obj_option(Objs.STAIRCASE_16673, option = "climb-down") {
     player.moveTo(3206, 3208, 1)
 }

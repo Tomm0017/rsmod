1043b43f128f742f37b769eb37728cfcaeac0b9f
pitchblac23 <tylermincks@hotmail.com>
2019-08-12 17:36:21 -0500

    added guild chest

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/bankchest/bank_chests.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/bankchest/bank_chests.plugin.kts
@@ -2,7 +2,7 @@ package gg.rsmod.plugins.content.objs.bankchest
 
 import gg.rsmod.plugins.content.inter.bank.openBank
 
-private val CHESTS = setOf(Objs.BANK_CHEST, Objs.BANK_CHEST_14886)
+private val CHESTS = setOf(Objs.BANK_CHEST, Objs.BANK_CHEST_14886, Objs.BANK_CHEST_28861)
 
 CHESTS.forEach { chests ->
     on_obj_option(obj = chests, option = "use") {

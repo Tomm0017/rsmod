236c1f06788e0f469e9fe3f85fd29168332ce9d7
pitchblac23 <tylermincks@hotmail.com>
2019-08-12 17:41:02 -0500

    added Darkk98's charges
    Thanks, Darkk98

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/amuletofglory/amulet_of_glory.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/amuletofglory/amulet_of_glory.plugin.kts
@@ -1,5 +1,10 @@
 package gg.rsmod.plugins.content.items.amuletofglory
 
+/**
+ * Thanks to Darkk98
+ * for making the inventory glory charges
+ **/
+
 import gg.rsmod.plugins.content.magic.TeleportType
 import gg.rsmod.plugins.content.magic.canTeleport
 import gg.rsmod.plugins.content.magic.teleport
@@ -28,13 +33,26 @@ GLORY.forEach { glory ->
             }
         }
     }
+    on_item_option(glory, option = 4) {
+        player.queue(TaskPriority.STRONG) {
+            when (options("Edgeville", "Karamja", "Draynor Village", "Al kharid", title = "Where Would You Like To Go")) {
+                1 -> player.teleport(Tile(3086, 3500), TeleportType.GLORY)
+                2 -> player.teleport(Tile(2917, 3176), TeleportType.GLORY)
+                3 -> player.teleport(Tile(3103, 3249), TeleportType.GLORY)
+                4 -> player.teleport(Tile(3292, 3165), TeleportType.GLORY)
+            }
+            player.inventory[player.getInteractingItemSlot()] = player.getGloryReplacement()
+            player.message(player.getGloryChargeMessage())
+        }
+    }
 }
 
-
 fun Player.teleport(endTile : Tile) {
-    if (canTeleport(TeleportType.GLORY) && hasEquipped(EquipmentType.AMULET, *GLORY)) {
+    if (canTeleport(TeleportType.GLORY)) {
         world.spawn(AreaSound(tile, SOUNDAREA_ID, SOUNDAREA_RADIUS, SOUNDAREA_VOLUME))
-        equipment[EquipmentType.AMULET.id] = getAmuletReplacement()
+        if(hasEquipped(EquipmentType.AMULET, *GLORY)) {
+            equipment[EquipmentType.AMULET.id] = getAmuletReplacement()
+        }
         message(getAmuletChargeMessage())
         teleport(endTile, TeleportType.GLORY)
     }
@@ -52,6 +70,18 @@ fun Player.getAmuletReplacement(): Item ? {
     }
 }
 
+fun Player.getGloryReplacement(): Item ? {
+    return when {
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY6) -> Item(Items.AMULET_OF_GLORY5)
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY5) -> Item(Items.AMULET_OF_GLORY4)
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY4) -> Item(Items.AMULET_OF_GLORY3)
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY3) -> Item(Items.AMULET_OF_GLORY2)
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY2) -> Item(Items.AMULET_OF_GLORY1)
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY1) -> Item(Items.AMULET_OF_GLORY)
+        else -> null
+    }
+}
+
 fun Player.getAmuletChargeMessage(): String {
     return when {
         hasEquipped(EquipmentType.AMULET, Items.AMULET_OF_GLORY5) -> "<col=7f007f>Your amulet has five charges left.</col>"
@@ -59,6 +89,17 @@ fun Player.getAmuletChargeMessage(): String {
         hasEquipped(EquipmentType.AMULET, Items.AMULET_OF_GLORY3) -> "<col=7f007f>Your amulet has three charges left.</col>"
         hasEquipped(EquipmentType.AMULET, Items.AMULET_OF_GLORY2) -> "<col=7f007f>Your amulet has two charges left.</col>"
         hasEquipped(EquipmentType.AMULET, Items.AMULET_OF_GLORY1) -> "<col=7f007f>Your amulet has one charge left.</col>"
+        else -> "<col=7f007f>You use your amulet's last charge.</col>"
+    }
+}
+
+fun Player.getGloryChargeMessage(): String {
+    return when {
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY5) -> "<col=7f007f>Your amulet has five charges left.</col>"
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY4) -> "<col=7f007f>Your amulet has four charges left.</col>"
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY3) -> "<col=7f007f>Your amulet has three charges left.</col>"
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY2) -> "<col=7f007f>Your amulet has two charges left.</col>"
+        inventory.hasAt(getInteractingItemSlot(), Items.AMULET_OF_GLORY1) -> "<col=7f007f>Your amulet has one charge left.</col>"
         else -> "<col=7f007f>You use your amulet's last charge.</col>"
     }
 }

440646b158e41c042cabd0623ed2d88d6a7fb761
pitchblac23 <tylermincks@hotmail.com>
2019-08-20 23:18:16 -0500

    edgevill dungeon
    added almost all the spawns for the dungeon area hill giant walk distance might need a tweak but its close includes all item spawns as well

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/edgeville/spawns/dungeon/spawns.plugin.kts
@@ -0,0 +1,96 @@
+package gg.rsmod.plugins.content.areas.edgeville.spawns.dungeon
+
+spawn_npc(npc = Npcs.GIANT_SPIDER_3017, x = 3096, z = 9878, walkRadius = 9)//17sec
+spawn_npc(npc = Npcs.GIANT_SPIDER_3017, x = 3097, z = 9878, walkRadius = 9)
+
+spawn_npc(npc = Npcs.RAT_2854, x = 3112, z = 9884, walkRadius = 11)
+spawn_npc(npc = Npcs.RAT_2854, x = 3124, z = 9882, walkRadius = 11)
+
+spawn_npc(npc = Npcs.GIANT_RAT_2864, x = 3117, z = 9889, walkRadius = 5)//17sec
+spawn_npc(npc = Npcs.GIANT_RAT_2863, x = 3122, z = 9889, walkRadius = 5)//17sec
+spawn_npc(npc = Npcs.GIANT_RAT_2863, x = 3089, z = 9895, walkRadius = 4)
+spawn_npc(npc = Npcs.GIANT_RAT_2856, x = 3121, z = 9893, walkRadius = 5)//17sec
+spawn_npc(npc = Npcs.GIANT_RAT_2856, x = 3102, z = 9880, walkRadius = 5)
+spawn_npc(npc = Npcs.GIANT_RAT_2856, x = 3102, z = 9880, walkRadius = 5)
+spawn_npc(npc = Npcs.GIANT_RAT_2856, x = 3091, z = 9897, walkRadius = 4)
+
+spawn_npc(npc = Npcs.SKELETON_81, x = 3141, z = 9869, walkRadius = 9)//35sec
+spawn_npc(npc = Npcs.SKELETON_80, x = 3144, z = 9869, walkRadius = 9)//35sec
+spawn_npc(npc = Npcs.SKELETON_79, x = 3141, z = 9873, walkRadius = 9)//35sec
+spawn_npc(npc = Npcs.SKELETON_78, x = 3121, z = 9908, walkRadius = 9)//35sec
+spawn_npc(npc = Npcs.SKELETON_78, x = 3139, z = 9877, walkRadius = 9)//35sec
+spawn_npc(npc = Npcs.SKELETON_77, x = 3121, z = 9908, walkRadius = 9)//35sec
+spawn_npc(npc = Npcs.SKELETON_77, x = 3134, z = 9911, walkRadius = 8)
+spawn_npc(npc = Npcs.SKELETON_77, x = 3137, z = 9871, walkRadius = 9)//35sec
+spawn_npc(npc = Npcs.SKELETON_76, x = 3094, z = 9889, walkRadius = 3)//21sec
+spawn_npc(npc = Npcs.SKELETON_76, x = 3098, z = 9906, walkRadius = 3)
+spawn_npc(npc = Npcs.SKELETON_75, x = 3096, z = 9893, walkRadius = 3)//21sec
+spawn_npc(npc = Npcs.SKELETON_75, x = 3099, z = 9912, walkRadius = 3)
+spawn_npc(npc = Npcs.SKELETON_74, x = 3095, z = 9899, walkRadius = 3)//21sec
+spawn_npc(npc = Npcs.SKELETON_74, x = 3095, z = 9909, walkRadius = 3)
+spawn_npc(npc = Npcs.SKELETON_73, x = 3132, z = 9914, walkRadius = 8)//41sec
+spawn_npc(npc = Npcs.SKELETON_73, x = 3138, z = 9874, walkRadius = 8)//41sec
+spawn_npc(npc = Npcs.SKELETON_72, x = 3128, z = 9909, walkRadius = 8)//41sec
+spawn_npc(npc = Npcs.SKELETON_71, x = 3114, z = 9910, walkRadius = 9)//41sec
+spawn_npc(npc = Npcs.SKELETON_71, x = 3132, z = 9908, walkRadius = 8)//41sec
+spawn_npc(npc = Npcs.SKELETON, x = 3132, z = 9904, walkRadius = 7)//41sec
+
+spawn_npc(npc = Npcs.HOBGOBLIN_3050, x = 3128, z = 9876, walkRadius = 3)//21sec
+spawn_npc(npc = Npcs.HOBGOBLIN_3050, x = 3123, z = 9879, walkRadius = 3)//21sec
+spawn_npc(npc = Npcs.HOBGOBLIN_3050, x = 3118, z = 9871, walkRadius = 3)//21sec
+spawn_npc(npc = Npcs.HOBGOBLIN_3049, x = 3128, z = 9876, walkRadius = 13)//21sec
+spawn_npc(npc = Npcs.HOBGOBLIN_3049, x = 3122, z = 9874, walkRadius = 13)//21sec
+
+spawn_npc(npc = Npcs.ZOMBIE_58, x = 3130, z = 9860, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_57, x = 3126, z = 9865, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_56, x = 3118, z = 9863, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_55, x = 3124, z = 9861, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_44, x = 3148, z = 9904, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_44, x = 3150, z = 9889, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_44, x = 3151, z = 9884, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_43, x = 3143, z = 9904, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_43, x = 3140, z = 9893, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_43, x = 3147, z = 9883, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_42, x = 3150, z = 9907, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_42, x = 3147, z = 9899, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_42, x = 3146, z = 9892, walkRadius = 9)//21sec
+spawn_npc(npc = Npcs.ZOMBIE_42, x = 3139, z = 9885, walkRadius = 9)//21sec
+
+spawn_npc(npc = Npcs.HILL_GIANT_2103, x = 3106, z = 9827, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2103, x = 3116, z = 9832, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2102, x = 3117, z = 9835, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2102, x = 3113, z = 9831, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2101, x = 3107, z = 9835, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2101, x = 3110, z = 9845, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2100, x = 3100, z = 9836, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2100, x = 3122, z = 9844, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2099, x = 3099, z = 9832, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT_2099, x = 3117, z = 9843, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3117, z = 9850, walkRadius = 4)//17sec
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3111, z = 9841, walkRadius = 4)//17sec
+
+
+spawn_item(Items.BONES, 1, x = 3093, z = 9879)
+spawn_item(Items.BONES, 1, x = 3093, z = 9884)
+spawn_item(Items.BONES, 1, x = 3098, z = 9886)
+spawn_item(Items.BONES, 1, x = 3122, z = 9891)
+spawn_item(Items.BONES, 1, x = 3120, z = 9894)
+spawn_item(Items.BONES, 1, x = 3119, z = 9894)
+spawn_item(Items.BONES, 1, x = 3116, z = 9891)
+spawn_item(Items.BONES, 1, x = 3094, z = 9906)
+spawn_item(Items.BONES, 1, x = 3097, z = 9901)
+spawn_item(Items.BONES, 1, x = 3138, z = 9880)
+spawn_item(Items.BONES, 1, x = 3141, z = 9879)
+spawn_item(Items.BONES, 1, x = 3142, z = 9880)
+spawn_item(Items.BONES, 1, x = 3143, z = 9878)
+spawn_item(Items.BONES, 1, x = 3110, z = 9825)
+spawn_item(Items.BONES, 1, x = 3109, z = 9823)
+spawn_item(Items.BONES, 1, x = 3107, z = 9823)
+spawn_item(Items.BONES, 1, x = 3101, z = 9825)
+spawn_item(Items.COINS_995, 1, x = 3091, z = 9899)
+spawn_item(Items.COINS_995, 1, x = 3088, z = 9899)
+spawn_item(Items.COINS_995, 4, x = 3088, z = 9898)
+spawn_item(Items.BRONZE_ARROW, 3, x = 3130, z = 9903)
+spawn_item(Items.BRONZE_ARROW, 1, x = 3135, z = 9916)
+spawn_item(Items.BRASS_NECKLACE, 1, x = 3122, z = 9881)
+spawn_item(Items.BRASS_KEY, 1, x = 3131, z = 9862)

034f820b7cfd2d1651710a5ca3d76fc707f857e5
pitchblac23 <tylermincks@hotmail.com>
2019-08-21 15:19:38 -0500

    fully spawned sewers
    will work on getting stats and anims and drops next

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/varrock/spawns/varrock_sewers.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/varrock/spawns/varrock_sewers.plugin.kts
@@ -1,6 +1,92 @@
 package gg.rsmod.plugins.content.areas.varrock.spawns
 
-//Moss Giants
+spawn_npc(Npcs.SPIDER_3019, 3261, 9869, walkRadius = 8)//20sec
+spawn_npc(Npcs.SPIDER_3019, 3262, 9870, walkRadius = 8)//20sec
+spawn_npc(Npcs.SPIDER_3019, 3262, 9868, walkRadius = 8)//20sec
+spawn_npc(Npcs.SPIDER_3019, 3210, 9893, walkRadius = 8)//20sec
+spawn_npc(Npcs.SPIDER_3019, 3212, 9891, walkRadius = 8)//20sec
+spawn_npc(Npcs.SPIDER_3019, 3210, 9887, walkRadius = 8)//20sec
+spawn_npc(Npcs.SPIDER_3019, 3212, 9888, walkRadius = 8)//20sec
+spawn_npc(Npcs.SPIDER_3019, 3206, 9880, walkRadius = 8)//20sec
+spawn_npc(Npcs.SPIDER_3019, 3201, 9877, walkRadius = 4)//20sec
+
+spawn_npc(Npcs.GIANT_SPIDER_3018, 3220, 9888, walkRadius = 10)//17sec
+spawn_npc(Npcs.GIANT_SPIDER_3018, 3217, 9886, walkRadius = 10)//17sec
+spawn_npc(Npcs.GIANT_SPIDER_3018, 3217, 9889, walkRadius = 10)//17sec
+spawn_npc(Npcs.GIANT_SPIDER_3017, 3221, 9886, walkRadius = 8)//17sec
+spawn_npc(Npcs.GIANT_SPIDER_3017, 3219, 9886, walkRadius = 8)//17sec
+
+spawn_npc(Npcs.RAT_2854, 3255, 9869, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3238, 9863, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3234, 9865, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3235, 9864, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3237, 9864, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3230, 9865, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3221, 9872, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3238, 9863, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3220, 9859, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3226, 9862, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3219, 9867, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3225, 9863, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3218, 9864, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3218, 9869, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3229, 9867, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3244, 9863, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3247, 9864, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3253, 9867, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3276, 9871, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3278, 9873, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3269, 9869, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3274, 9873, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3238, 9863, walkRadius = 15)
+spawn_npc(Npcs.RAT_2854, 3242, 9895, walkRadius = 11)
+spawn_npc(Npcs.RAT_2854, 3251, 9893, walkRadius = 10)
+spawn_npc(Npcs.RAT_2854, 3244, 9875, walkRadius = 9)
+spawn_npc(Npcs.RAT_2854, 3244, 9886, walkRadius = 9)
+spawn_npc(Npcs.RAT_2854, 3242, 9893, walkRadius = 9)
+spawn_npc(Npcs.RAT_2854, 3252, 9892, walkRadius = 9)
+
+spawn_npc(Npcs.GIANT_RAT_2864, 3223, 9860, walkRadius = 5)//17sec
+spawn_npc(Npcs.GIANT_RAT_2863, 3239, 9864, walkRadius = 5)//17sec
+spawn_npc(Npcs.GIANT_RAT_2863, 3279, 9894, walkRadius = 6)//17sec
+spawn_npc(Npcs.GIANT_RAT_2862, 3283, 9896, walkRadius = 6)//17sec
+spawn_npc(Npcs.GIANT_RAT_2856, 3220, 9863, walkRadius = 5)//17sec
+spawn_npc(Npcs.GIANT_RAT_2856, 3233, 9866, walkRadius = 5)//17sec
+
+spawn_npc(Npcs.SCORPION_3024, 3253, 9903, walkRadius = 8)//15sec
+spawn_npc(Npcs.SCORPION_3024, 3251, 9908, walkRadius = 8)//15sec
+
+spawn_npc(Npcs.ZOMBIE_58, 3230, 9903, walkRadius = 9)//21sec
+spawn_npc(Npcs.ZOMBIE_57, 3230, 9906, walkRadius = 9)//21sec
+spawn_npc(Npcs.ZOMBIE_56, 3226, 9903, walkRadius = 9)//21sec
+spawn_npc(Npcs.ZOMBIE_55, 3224, 9906, walkRadius = 9)//21sec
+spawn_npc(Npcs.ZOMBIE_41, 3243, 9893, walkRadius = 9)//21sec
+spawn_npc(Npcs.ZOMBIE_39, 3259, 9891, walkRadius = 9)//21sec
+
+spawn_npc(Npcs.SKELETON_80, 3211, 9906, walkRadius = 9)//21sec
+spawn_npc(Npcs.SKELETON_77, 3210, 9908, walkRadius = 8)//35sec
+spawn_npc(Npcs.SKELETON_76, 3275, 9911, walkRadius = 3)//21sec
+spawn_npc(Npcs.SKELETON_76, 3253, 9916, walkRadius = 3)//21sec
+spawn_npc(Npcs.SKELETON_75, 3274, 9913, walkRadius = 3)//21sec
+spawn_npc(Npcs.SKELETON_75, 3251, 9916, walkRadius = 3)//21sec
+spawn_npc(Npcs.SKELETON_74, 3270, 9914, walkRadius = 3)//21sec
+spawn_npc(Npcs.SKELETON_74, 3275, 9909, walkRadius = 3)//21sec
+spawn_npc(Npcs.SKELETON_74, 3248, 9914, walkRadius = 3)//21sec
+
+spawn_npc(Npcs.GHOST_92, 3243, 9915, walkRadius = 6)//24sec
+spawn_npc(Npcs.GHOST_89, 3239, 9915, walkRadius = 6)//24sec
+spawn_npc(Npcs.GHOST, 3241, 9913, walkRadius = 6)//24sec
+
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3186, 9892, walkRadius = 10)//17sec
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3178, 9888, walkRadius = 10)//17sec
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3176, 9883, walkRadius = 10)//17sec
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3175, 9881, walkRadius = 10)//17sec
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3178, 9880, walkRadius = 10)//17sec
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3180, 9883, walkRadius = 10)//17sec
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3183, 9885, walkRadius = 10)//17sec
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3174, 9891, walkRadius = 10)//17sec
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 3171, 9884, walkRadius = 10)//17sec
+
 spawn_npc(Npcs.MOSS_GIANT_2090, 3163,9877,0,1, Direction.SOUTH)
 spawn_npc(Npcs.MOSS_GIANT_2090, 3158,9899,0,1, Direction.NORTH_EAST)
 spawn_npc(Npcs.MOSS_GIANT_2090, 3156,9907,0,1, Direction.NORTH_WEST)
@@ -12,13 +98,13 @@ spawn_npc(Npcs.MOSS_GIANT_2092, 3155,9904,0,1, Direction.EAST)
 spawn_npc(Npcs.MOSS_GIANT_2093, 3166,9883,0,1, Direction.NORTH_WEST)
 spawn_npc(Npcs.MOSS_GIANT_2093, 3159,9902,0,1, Direction.NORTH_EAST)
 
-//items
 spawn_item(Items.IRON_AXE, 1, 3162,9888,0,100)
 spawn_item(Items.COINS_995, 5, 3169,9880,0,100)
 spawn_item(Items.COINS_995, 7, 3173,9881,0,100)
 spawn_item(Items.COINS_995, 1, 3173,9897,0,100)
 spawn_item(Items.COINS_995, 1, 3274,9870,0,100)
 spawn_item(Items.COINS_995, 8, 3167,9897,0,100)
+spawn_item(Items.COINS_995, 1, 3274,9870,0,100)
 spawn_item(Items.BONES, 1, 3173,9903,0,1)
 spawn_item(Items.RED_SPIDERS_EGGS, 1, 3179,9881,0,100)
 spawn_item(Items.RED_SPIDERS_EGGS, 1, 3177,9880,0,100)

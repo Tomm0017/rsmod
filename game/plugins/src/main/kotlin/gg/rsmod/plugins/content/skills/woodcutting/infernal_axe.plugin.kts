e587c45e5bb24b43d55b04f7dd35e0436535607e
pitchblac23 <tylermincks@hotmail.com>
2019-09-07 19:24:12 -0500

    fix for recharging
    fixed being able to recharge infernal axe with stone or d axe

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/woodcutting/infernal_axe.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/woodcutting/infernal_axe.plugin.kts
@@ -1,13 +1,20 @@
 package gg.rsmod.plugins.content.skills.woodcutting
 
+import gg.rsmod.plugins.api.cfg.Items
+
 on_item_on_item(item1 = Items.DRAGON_AXE, item2 = Items.SMOULDERING_STONE) {
     Woodcutting.createAxe(player)
 }
-on_item_on_item(item1 = Items.INFERNAL_AXE_UNCHARGED, item2 = Items.SMOULDERING_STONE) {
-    player.inventory.remove(Items.INFERNAL_AXE_UNCHARGED)
-    player.inventory.remove(Items.SMOULDERING_STONE)
-    player.inventory.add(Items.INFERNAL_AXE)
-    Woodcutting.charges = 5000
+
+val rechargeItem = intArrayOf(Items.SMOULDERING_STONE, Items.DRAGON_AXE)
+
+rechargeItem.forEach { item ->
+    on_item_on_item(item1 = Items.INFERNAL_AXE_UNCHARGED, item2 = item) {
+        player.inventory.remove(Items.INFERNAL_AXE_UNCHARGED)
+        player.inventory.remove(item)
+        player.inventory.add(Items.INFERNAL_AXE)
+        Woodcutting.charges = 5000
+    }
 }
 
 on_item_option(Items.INFERNAL_AXE, "check") {

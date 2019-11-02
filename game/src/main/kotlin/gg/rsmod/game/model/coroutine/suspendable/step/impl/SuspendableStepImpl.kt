4e59228d4f31165512e4ab79ae8fec1d1a793e62
Curtis Woodard <nbness2@gmail.com>
2019-11-02 15:50:13 -0600

    Added `SuspendableStepImpl`, a sample implementation of `SuspendableStep`

---
+++ game/src/main/kotlin/gg/rsmod/game/model/coroutine/suspendable/step/impl/SuspendableStepImpl.kt
@@ -0,0 +1,20 @@
+package gg.rsmod.game.model.coroutine.suspendable.step.impl
+
+import gg.rsmod.game.model.coroutine.suspendable.condition.SuspendableCondition
+import gg.rsmod.game.model.coroutine.suspendable.step.SuspendableStep
+import kotlin.coroutines.Continuation
+
+/**
+ * A Sample implementation of [SuspendableStep]
+ *
+ * @property [condition] A [SuspendableCondition] (possible because [SuspendableCondition] extends `() -> Unit`) that
+ *      decides whether or not to step on to the next step of this [SuspendableStep]
+ *
+ * @property [continuation] The [Continuation] that you are stepping over.
+ *
+ * @author Curtis Woodard <Nbness2@gmail.com>
+ */
+class SuspendableStepImpl(
+        override val condition: SuspendableCondition,
+        override val continuation: Continuation<Unit>
+): SuspendableStep

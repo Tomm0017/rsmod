8fad6a8563e68baeb6c66ff491eb5ebde16433c9
Curtis Woodard <nbness2@gmail.com>
2019-11-02 15:39:41 -0600

    Added `interface SuspendableStep`

---
+++ game/src/main/kotlin/gg/rsmod/game/model/coroutine/suspendable/SuspendableStep.kt
@@ -0,0 +1,24 @@
+package gg.rsmod.game.model.coroutine.suspendable
+
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+
+/**
+ * Represents a continuation that can be suspended and stepped over in the future depending on a condition.
+ *
+ * @author Nbness2 <Nbness2@gmail.com>
+ */
+interface SuspendableStep {
+
+    /** [condition] Whether or not to resume this step. */
+    val condition: () -> Boolean
+
+    /** [continuation] The [Continuation] to step over. */
+    val continuation: Continuation<Unit>
+
+    /** Whether or not to resume stepping over [continuation] */
+    fun canResume() = condition()
+
+    /** Step over the next step of [continuation] */
+    fun resume() = continuation.resume(Unit)
+}

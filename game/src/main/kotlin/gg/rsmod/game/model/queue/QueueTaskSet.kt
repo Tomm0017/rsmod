3667358bd5dccae5f1d4c9151b18b414964a677d
Curtis Woodard <nbness2@gmail.com>
2019-11-06 14:17:42 -0700

    `QueueTaskSet` now implements `CyclableTaskSet<QueueTask>`

--- game/src/main/kotlin/gg/rsmod/game/model/queue/QueueTaskSet.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/queue/QueueTaskSet.kt
@@ -1,24 +1,21 @@
 package gg.rsmod.game.model.queue
 
+import gg.rsmod.game.model.coroutine.cyclable.set.CyclableTaskSet
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import java.util.*
 import kotlin.coroutines.createCoroutine
 
-/**
- * A system responsible for task coroutine logic.
- *
- * @author Tom <rspsmods@gmail.com>
- */
-abstract class QueueTaskSet {
+abstract class QueueTaskSet: CyclableTaskSet<QueueTask> {
 
-    protected val queue: LinkedList<QueueTask> = LinkedList()
+    override val queue: LinkedList<QueueTask> = LinkedList()
 
-    val size: Int get() = queue.size
-
-    abstract fun cycle()
-
-    fun queue(ctx: Any, dispatcher: CoroutineDispatcher, priority: TaskPriority, block: suspend QueueTask.(CoroutineScope) -> Unit) {
+    override fun queue(
+            ctx: Any,
+            dispatcher: CoroutineDispatcher,
+            priority: TaskPriority,
+            block: suspend QueueTask.(CoroutineScope) -> Unit
+    ) {
         val task = QueueTask(ctx, priority)
         val suspendBlock = suspend { block(task, CoroutineScope(dispatcher)) }
 
@@ -29,26 +26,5 @@ abstract class QueueTaskSet {
         }
 
         queue.addFirst(task)
-    }
-
-    /**
-     * In-game events sometimes must return a value to a plugin. An example are
-     * dialogs which must return values such as input, button click, etc.
-     *
-     * @param value
-     * The return value that the plugin has asked for.
-     */
-    fun submitReturnValue(value: Any) {
-        val task = queue.peek() ?: return // Shouldn't call this method without a queued task.
-        task.requestReturnValue = value
-    }
-
-    /**
-     * Remove all [QueueTask] from our [queue], invoking each task's [QueueTask.terminate]
-     * before-hand.
-     */
-    fun terminateTasks() {
-        queue.forEach { it.terminate() }
-        queue.clear()
     }
 }

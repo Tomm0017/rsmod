1d07222cedd52f0b2d90b00563a449dd4ebbc0a1
Curtis Woodard <github/nbness2>
2019-11-09 11:33:40 -0700

    Made `AttributeDelegate.defaultValue` mandatory

--- game/src/main/kotlin/gg/rsmod/game/model/attr/AttributeDelegate.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/attr/AttributeDelegate.kt
@@ -3,16 +3,13 @@ package gg.rsmod.game.model.attr
 import gg.rsmod.game.model.entity.Pawn
 import kotlin.reflect.KProperty
 
-class AttributeDelegate<T>(val attributeKey: AttributeKey<T>, private val defaultValue: T? = null) {
-    constructor(persistenceKey: String? = null, resetOnDeath: Boolean = false, defaultValue: T? = null): this(
+class AttributeDelegate<T>(val attributeKey: AttributeKey<T>, private val defaultValue: T) {
+    constructor(persistenceKey: String? = null, resetOnDeath: Boolean = false, defaultValue: T): this(
             attributeKey = AttributeKey<T>(persistenceKey, resetOnDeath),
             defaultValue = defaultValue
     )
 
-    operator fun getValue(pawn: Pawn, prop: KProperty<*>): T =
-            pawn.attr[attributeKey] ?:
-            defaultValue ?:
-            throw Exception("Attribute not found in pawn $pawn and has no default value")
+    operator fun getValue(pawn: Pawn, prop: KProperty<*>): T = pawn.attr[attributeKey] ?: defaultValue
 
     operator fun setValue(pawn: Pawn, prop: KProperty<*>, newValue: T) { pawn.attr[attributeKey] = newValue }
 }
